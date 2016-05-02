42a83d90c2c8618f292ee5cfcb8bfdb89cd481d1
jorgejcabrera <cabrerajjorge@gmail.com>
2016-05-02 00:05:01 -0300

    mips funcion call

--- Factorial/Source/Factorial.c
+++ Factorial/Source/Factorial.c
@@ -11,12 +11,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int  factorial (unsigned int n){
+/*unsigned int  factorial (unsigned int n){
 	if (n < 2)
 		return 1;
 	else
 		return n*factorial(n-1);
-}
+}*/
+
+extern void multiplyMatrices(unsigned int n);
 
 int main(void) {
 	printf("%d",factorial(5));
