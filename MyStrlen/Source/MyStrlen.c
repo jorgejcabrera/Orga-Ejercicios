3d6678e3a4ba511274f7c011ed8d0e1a1ed62f4f
jorgejcabrera <cabrerajjorge@gmail.com>
2016-06-21 00:15:45 -0300

    implementado my_strlen_utf8 en mips

--- MyStrlen/Source/MyStrlen.c
+++ MyStrlen/Source/MyStrlen.c
@@ -11,19 +11,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int my_strlen_utf8_c(char *s){
+/*int my_strlen_utf8_c(char *s){
 	int i = 0, j = 0;
 	while (s[i]) {
 		if ((s[i] & 0xc0) != 0x80) j++;
 		i++;
 	}
 	return j;
-}
+}*/
+
+extern void my_strlen_utf8_c(char *s);
 
 int main(void) {
 	char* myString = "my string is too long";
 	int myStringLen = my_strlen_utf8_c(myString);
 	printf("%d\n", myStringLen);
-
 	return EXIT_SUCCESS;
 }
